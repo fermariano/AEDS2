e2a6edf20f125d44af0233aee6231b9151730015
fernanda <121069175+fermariano@users.noreply.github.com>
2023-08-27 22:02:50 -0300

    Add files via upload

---
+++ TT5/ArrayList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+public class Program {
+    public static void Main() {
+
+        ArrayList vetor = new ArrayList();
+
+        Console.WriteLine("Capacidade: " + vetor.Capacity);
+        Console.WriteLine("Número de elementos: " + vetor.Count);
+
+        Console.WriteLine("------------");
+
+        vetor.Add(4);
+
+        Console.WriteLine("Capacidade: " + vetor.Capacity);
+        Console.WriteLine("Número de elementos: " + vetor.Count);
+
+    }
+}
